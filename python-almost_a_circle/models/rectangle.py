86e351d836ec80f644737b67ffe114fb02031adf
Washx23 <5731@holbertonstudents.com>
2023-06-30 21:07:28 -0700

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """document"""
 
+
 from models.base import Base
 
 
-class rectangle(Base):
+class Rectangle(Base):
     """rectangle class"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
 
         self.__width = width
